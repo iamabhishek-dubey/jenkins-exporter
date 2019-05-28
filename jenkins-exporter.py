440e39a8711008ba905053f69f5c6a76291ca233
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-05-28 14:32:14 +0530

    Updated import library

--- jenkins-exporter.py
+++ jenkins-exporter.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+from httplib2 import Http
+import base64
 import json
-import re
-import sys
 import time
-import urllib2
-
+import os
+import signal
+import faulthandler
+from threading import Lock
+from urllib.parse import urlencode, quote_plus
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
+import logging
+from pythonjsonlogger import jsonlogger
 
